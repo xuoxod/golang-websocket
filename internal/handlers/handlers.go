6ea834df8fc58eedbcc7f598d2441c630a848973
rick xuoxod <xuoxod@gmail.com>
2023-10-18 19:50:50 -0400

    Coded message sending

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -120,6 +120,11 @@ func ListenToWsChannel() {
 			users := getUserList()
 			response.ConnectedUsers = users
 			broadcastToAll(response)
+
+		case "broadcast":
+			response.Action = "broadcast"
+			response.Message = fmt.Sprintf("<strong>%s</strong>:\t%s", e.Username, e.Message)
+			broadcastToAll(response)
 		}
 
 		// response.Action = "Got here"
